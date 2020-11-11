a840cad399fd2d53cbad9afbad0a4d7f46812cb1
Guenther Brunthaler <gb@xduo.emgenxx69lwyn5ctlr4nl64ul.local>
2020-11-11 06:10:40 +0100

    Support command line arguments
    All non-option arguments are expected to be pathnames of binary files to
    be hashed. In this case, the output for each file will consist of its
    message digest in base-32 format and the path name, separated by a
    single space.
    
    Only a single option is supported: "--" which stops processing of
    further options.
    
    Options are only recognized before all non-option arguments.
    
    Without any options, the program behaves as before and just hashes its
    standard input stream.

--- mdarc4.c
+++ mdarc4.c
@@ -1,7 +1,7 @@
 /*
  * mdarc4 - abuse ARC4 as a simplistic and reasonably fast hash algorithm
  *
- * Version 2020.316.2
+ * Version 2020.316.3
  *
  * Copyright (c) 2020 Guenther Brunthaler. All rights reserved.
  *
@@ -44,65 +44,87 @@ static char const alphabet[]= {
 #define ARC4_STEP_5 v1= s[i]; s[i]= v2= s[j]; s[j]= v1
 #define ARC4_STEP_6() s[SBOX_MOD(v1 + v2)]
 
-int main(void) {
+int main(int argc, char **argv) {
    static unsigned char s[SBOX_SIZE];
    char const *error= 0;
+   int a;
    unsigned i, j;
    unsigned char v1, v2;
    /* Ensure SBOX_SIZE is an integral power of 2. */
    ASSERT_POWER_OF_2(SBOX_SIZE);
    /* Ensure correct alphabet size. */
    assert(DIM(alphabet) == 1 << ALPHABET_BITS);
-   ARC4_STEP_1;
-   ARC4_STEP_2;
-   /* Process input as an (overly long) key to set. */
-   {
-      int c;
-      while ((c= getchar()) != EOF) {
-         ARC4_STEP_3;
-         assert(c >= 0); assert(c < SBOX_SIZE);
-         ARC4_STEP_4_SETUP((unsigned)c);
-         ARC4_STEP_5_DROP;
+   if ((a= 1) < argc && argv[a][0] == '-') {
+      if (argv[a][1] != '-' || argv[a][2] != '\0') {
+         error= "Unsupported option!"; goto fail;
       }
+      ++a;
    }
-   if (ferror(stdin)) { error= "Read error!"; goto fail; }
-   assert(feof(stdin));
-   /* Finish key setup. */
-   ARC4_STEP_2;
-   /* Drop the initial keystream. */
-   {
-      unsigned k;
-      for (k= DROP_N; k--; ) {
-         ARC4_STEP_3;
-         ARC4_STEP_4;
-         ARC4_STEP_5_DROP;
+   for (;;) {
+      if (a < argc) {
+         if (!freopen(argv[a], "rb", stdin)) {
+            (void)fputs("Could not open \"", stderr);
+            (void)fputs(argv[a], stderr);
+            error= "\"!"; goto fail;
+         }
       }
-   }
-   /* Produce the message digest. */
-   {
-      int k;
-      unsigned buf, bufbits= 0;
-      #ifndef NDEBUG
-         buf= 0;
-      #endif
-      assert(DIGEST_BITS % 8 == 0);
-      for (k= DIGEST_BITS; k > 0; k-= ALPHABET_BITS) {
-         if (bufbits < ALPHABET_BITS) {
-            /* Append the bits of another ARCFOUR output octet to <buf>. */
+      /* Hash current standard input */
+      ARC4_STEP_1;
+      ARC4_STEP_2;
+      /* Process input as an (overly long) key to set. */
+      {
+         int c;
+         while ((c= getchar()) != EOF) {
+            ARC4_STEP_3;
+            assert(c >= 0); assert(c < SBOX_SIZE);
+            ARC4_STEP_4_SETUP((unsigned)c);
+            ARC4_STEP_5_DROP;
+         }
+      }
+      if (ferror(stdin)) { error= "Read error!"; goto fail; }
+      assert(feof(stdin));
+      /* Finish key setup. */
+      ARC4_STEP_2;
+      /* Drop the initial keystream. */
+      {
+         unsigned k;
+         for (k= DROP_N; k--; ) {
             ARC4_STEP_3;
             ARC4_STEP_4;
-            ARC4_STEP_5;
-            buf= buf << 8 | ARC4_STEP_6();
-            bufbits+= 8;
-         }
-         assert(bufbits >= ALPHABET_BITS);
-         {
-            int c= alphabet[ALPHABET_MOD(buf >> bufbits - ALPHABET_BITS)];
-            bufbits-= ALPHABET_BITS;
-            if (putchar(c) != c) goto wrerr;
+            ARC4_STEP_5_DROP;
          }
       }
-      if (putchar('\n') != '\n') goto wrerr;
+      /* Produce the message digest. */
+      {
+         int k;
+         unsigned buf, bufbits= 0;
+         #ifndef NDEBUG
+            buf= 0;
+         #endif
+         assert(DIGEST_BITS % 8 == 0);
+         for (k= DIGEST_BITS; k > 0; k-= ALPHABET_BITS) {
+            if (bufbits < ALPHABET_BITS) {
+               /* Append the bits of another ARCFOUR output octet to <buf>. */
+               ARC4_STEP_3;
+               ARC4_STEP_4;
+               ARC4_STEP_5;
+               buf= buf << 8 | ARC4_STEP_6();
+               bufbits+= 8;
+            }
+            assert(bufbits >= ALPHABET_BITS);
+            {
+               int c= alphabet[ALPHABET_MOD(buf >> bufbits - ALPHABET_BITS)];
+               bufbits-= ALPHABET_BITS;
+               if (putchar(c) != c) goto wrerr;
+            }
+         }
+         if (a < argc) {
+            if (putchar(' ') == EOF) goto wrerr;
+            if (fputs(argv[a], stdout) < 0) goto wrerr;
+         }
+         if (putchar('\n') == EOF) goto wrerr;
+      }
+      if (!(++a < argc)) break;
    }
    if (fflush(0)) {
       wrerr: error= "Write error!";

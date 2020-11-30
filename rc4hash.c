781a55a90120e4ac91bd236695a4c140471b2335
Guenther Brunthaler <gb@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2020-11-30 22:06:46 +0100

    Add online-help to rc4hash
    The -h and -V options shall display help and version information.
    
    Include an option for directly specifying the desired output width of
    the hash digest as a number of characters.
    
    This is preferable to the existing specification as a desired number of
    bits or bytes in cases where the output width of the hash has to be
    chosen in order to fit into a predefined column layout for publishing.

--- rc4hash.c
+++ rc4hash.c
@@ -1,13 +1,116 @@
-/*
- * rc4hash - abuse ARCFOUR as a simplistic and reasonably fast hash algorithm
- *
- * Version 2020.322.4
- *
- * Copyright (c) 2020 Guenther Brunthaler. All rights reserved.
- *
- * This source file is free software.
- * Distribution is permitted under the terms of the GPLv3.
- */
+#define VERSTR_1 "Version 2020.335"
+#define VERSTR_2 "Copyright (c) 2020 Guenther Brunthaler."
+
+static char help[]= { /* Formatted as 66 output columns. */
+   "rc4hash - abuse ARCFOUR for hashing or for key stretching\n"
+   "\n"
+   "The program abuses the ARCFOUR-drop3072 stream cipher as a\n"
+   "cryptographically non-secure hash algorithm which can generate a\n"
+   "hash value of any desired size.\n"
+   "\n"
+   "This allows the program also to be used for key stretching by\n"
+   "hashing the key into a hash value with the same size as the\n"
+   "desired stretched key.\n"
+   "\n"
+   "'non-secure' means that the algorithm has not been checked\n"
+   "whether it can withstand an attacker changing the hashed message\n"
+   "in such a way that the same hash value results even though it is\n"
+   "a different message now. Maybe it is secure in this regard; maybe\n"
+   "not. Better assume the latter.\n"
+   "\n"
+   "The same is true as part of a HMAC construction with arbitrarily\n"
+   "defined input block size, although it seems more likely that this\n"
+   "might in fact be sufficiently secure.\n"
+   "\n"
+   "The program can however be used as a sufficiently secure regular\n"
+   "checksum. For very small output sizes like 128 bit and less,\n"
+   "regular CRCs should be preferred because they have mathematical\n"
+   "guarantees about error detection which this program cannot\n"
+   "provide.\n"
+   "\n"
+   "On the other hand, this program should be much faster than a\n"
+   "cryptographically secure hash algorithm like MD5 or the\n"
+   "SHA-variants while providing the same (actually: arbitrary) hash\n"
+   "sizes as output. It might be slower for very small inputs though,\n"
+   "because it has a relatively high startup overhead.\n"
+   "\n"
+   "Usage: rc4hash [ <options> ] [ <file1> ... ]\n"
+   "\n"
+   "where <file1> is the pathname of the first file to be hashed. If\n"
+   "no files are specified at all, the program hashes all data sent\n"
+   "to its standard input.\n"
+   "\n"
+   "The following <options> are supported:\n"
+   "\n"
+   "-x: The hash output uses hexadecimal representation, much like\n"
+   "the output of the widely-employed 'sha256' utility. By default,\n"
+   "the hash is written using a custom base-32 representation which\n"
+   "is somewhat shorter than the output generated with -x.\n"
+   "\n"
+   "-r: Output the hash as raw binary 8-bit bytes (octets). This is\n"
+   "not human-readable, but the most compact representation. Use it\n"
+   "for key stretching where a binary stretched key is required.\n"
+   "\n"
+   "-b <bits>: Specify the desired digest size in bits. Will be\n"
+   "rounded up to the next multiple of the output alphabet bit size\n"
+   "(8 for option -r, 4 for -x, otherwise 5). The default output size\n"
+   "is '-b 256'.\n"
+   "\n"
+   "-B <octets>: Specify the desired digest size as 8-bit bytes. This\n"
+   "is the same as -b with with 8 times the argument value.\n"
+   "\n"
+   "-c <chars>: Specify the desired digest size directly as a number\n"
+   "of output characters in the hash representation. The hash\n"
+   "algorithm itself generates an endless number of output characters\n"
+   "as the hash. Output stops when <chars> characters have been\n"
+   "written as the digest representation.\n"
+   "\n"
+   "-h: Display this help and exit.\n"
+   "\n"
+   "-V: Display version information and exit.\n"
+   "\n"
+   "Note that although the algorithm can create hashes of any\n"
+   "requested output size, it only has an internal state of\n"
+   "floor(log2(256!)) bits, i. e. 1683 bits. While it will happily\n"
+   "output hash digests much larger than this, the amount of\n"
+   "information actually represented by the those digests will never\n"
+   "exceed 1683 bits.\n"
+   "\n"
+   "There are several reasons why this program exists:\n"
+   "\n"
+   "* Claims of being cryptographically secure is not one of them\n"
+   "\n"
+   "* The algorithm is fast, at least for larger inputs (because of\n"
+   "the relatively high startup overhead per input file/stream)\n"
+   "\n"
+   "* It is based on a well known algorithm, ARCFOUR, which has known\n"
+   "deficiencies for certain usage cases but is otherwise still\n"
+   "considered secure enough.\n"
+   "\n"
+   "* ARCFOUR is so simple that it seems highly unlikely that its\n"
+   "designers could have managed to put an algorithmic backdoor into\n"
+   "its design.\n"
+   "\n"
+   "* It is even so simple that the whole algorithm can be known be\n"
+   "heart. A developer can remember all details required for later\n"
+   "re-implementation without too much difficulty, even without\n"
+   "access to the specification, existing source code or any kind of\n"
+   "network. This might be an advantage in repressive regimes where\n"
+   "cryptography is banned and cryptographic source code cannot be\n"
+   "distributed without severe consequences. But as long as they\n"
+   "cannot wipe the memories of their citizens, this program may\n"
+   "still be re-implemented there from scratch.\n"
+   "\n"
+};
+
+static char version_info[]= {
+   VERSTR_1 "\n"
+   "\n"
+   VERSTR_2 " All rights reserved.\n"
+   "\n"
+   "This program is free software.\n"
+   "Distribution is permitted under the terms of the GPLv3."
+};
 
 #include "arc4_common.h"
 #include <dim_sdbrke8ae851uitgzm4nv3ea2.h>
@@ -36,12 +139,13 @@ static char const hex_alphabet[]= {
 int main(int argc, char **argv) {
    char const *error= 0;
    int a= 0;
-   long digest_bits= 256;
+   long digest_chars= 0;
    char const *alphabet= b32custom_alphabet;
    int alphabet_bitmask= (int)DIM(b32custom_alphabet) - 1, alphabet_bits;
    ARCFOUR_VARDEFS(static);
    {
       int optpos= 0;
+      long digest_bits= 256;
       for (;;) {
          int opt;
          switch (opt= getopt_simplest(&a, &optpos, argc, argv)) {
@@ -51,7 +155,7 @@ int main(int argc, char **argv) {
                alphabet_bitmask= (int)DIM(hex_alphabet) - 1;
                break;
             case 'r': alphabet= 0; alphabet_bitmask= (1 << 8) - 1; break;
-            case 'B': case 'b':
+            case 'b': case 'B': case 'c':
                {
                   char const *optarg;
                   if (!(optarg= getopt_simplest_mand_arg(
@@ -59,27 +163,37 @@ int main(int argc, char **argv) {
                   ))) {
                      getopt_simplest_perror_missing_arg(opt); goto leave;
                   }
-                  if ((digest_bits= atol(optarg)) < 1) goto bad_digest_size;
-                  if (opt == 'B') {
-                     long old= digest_bits;
-                     if ((digest_bits<<= 3) <= old) goto bad_digest_size;
+                  if ((digest_bits= atol(optarg)) < 1) {
+                     bad_digest_size:
+                     error= "Invalid digest size requested!"; goto fail;
                   }
                }
+               switch (opt) {
+                  case 'c':
+                     digest_chars= digest_bits; digest_bits= 0;
+                     break;
+                  case 'B':
+                     {
+                        long old= digest_bits;
+                        if ((digest_bits<<= 3) <= old) goto bad_digest_size;
+                     }
+               }
                break;
+            case 'h': (void)fputs(help, stderr); /* Fall through. */
+            case 'V': error= version_info; goto fail;
             default: getopt_simplest_perror_opt(opt); goto leave;
          }
       }
-   }
-   no_more_options:
-   {
-      int bm;
-      for (alphabet_bits= bm= 0; bm != alphabet_bitmask; ++alphabet_bits) {
-         bm+= bm + 1;
+      no_more_options:
+      {
+         int bm;
+         for (alphabet_bits= bm= 0; bm != alphabet_bitmask; ++alphabet_bits) {
+            bm+= bm + 1;
+         }
       }
-   }
-   if (digest_bits & alphabet_bits - 1) {
-      bad_digest_size:
-      error= "Invalid digest size requested!"; goto fail;
+      if (digest_bits) {
+         digest_chars= (digest_bits + alphabet_bits - 1) / alphabet_bits;
+      }
    }
    for (;;) {
       if (a < argc) {
@@ -130,7 +244,7 @@ int main(int argc, char **argv) {
          #ifndef NDEBUG
             buf= 0;
          #endif
-         for (k= digest_bits; k > 0; k-= alphabet_bits) {
+         for (k= digest_chars; k--; ) {
             if (bufbits < alphabet_bits) {
                /* Append the bits of another ARCFOUR output octet to <buf>. */
                ARCFOUR_STEP_3;

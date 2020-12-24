ee99feb0b6c8aa753e1a8fe9288bb28a7508953d
Guenther Brunthaler <gb@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2020-12-24 04:08:23 +0100

    Preparation for MAC calculation
    Support multiple ARCFOUR instance contexts to co-exist at the same time
    by putting all related variables into a single struct.

--- arc4_common.h
+++ arc4_common.h
@@ -2,7 +2,7 @@
  * C Macros implementing the basic ARCFOUR algorithm for shared use in
  * different applications.
  *
- * Version 2020.355
+ * Version 2020.359
  *
  * Copyright (c) 2020 Guenther Brunthaler. All rights reserved.
  *
@@ -16,15 +16,21 @@
 #define SBOX_SIZE (1 << 8)
 #define SBOX_MOD(x) ((x) & SBOX_SIZE - 1)
 
-#define ARCFOUR_VARDEFS(stclass) \
-   stclass unsigned char s[SBOX_SIZE], v1, v2; \
-   unsigned i, j
-#define ARCFOUR_STEP_1_KEY for (i= SBOX_SIZE; i-- ;) s[i]= (unsigned char)i
-#define ARCFOUR_STEP_2 i= j= 0
-#define ARCFOUR_STEP_3_PRNG i= SBOX_MOD(i + 1)
-#define ARCFOUR_STEP_4_KEY(keyoctet) j= SBOX_MOD(j + s[i] + keyoctet)
-#define ARCFOUR_STEP_4_PRNG j= SBOX_MOD(j + s[i])
-#define ARCFOUR_STEP_5_DROP v1= s[i]; s[i]= s[j]; s[j]= v1
-#define ARCFOUR_STEP_5_PRNG v1= s[i]; s[i]= v2= s[j]; s[j]= v1
-#define ARCFOUR_STEP_6_PRNG() s[SBOX_MOD(v1 + v2)]
-#define ARCFOUR_STEP_7_KEY i= SBOX_MOD(i + 1)
+#define ARCFOUR_VARDEFS(storage_class) \
+   storage_class struct { \
+      unsigned char s[SBOX_SIZE], v1, v2; \
+      unsigned i, j; \
+   } r4
+#define ARCFOUR_STEP_1_KEY for (r4.i= SBOX_SIZE; r4.i-- ; ) \
+   r4.s[r4.i]= (unsigned char)r4.i
+#define ARCFOUR_STEP_2 r4.i= r4.j= 0
+#define ARCFOUR_STEP_3_PRNG r4.i= SBOX_MOD(r4.i + 1)
+#define ARCFOUR_STEP_4_KEY(keyoctet) \
+   r4.j= SBOX_MOD(r4.j + r4.s[r4.i] + (keyoctet))
+#define ARCFOUR_STEP_4_PRNG r4.j= SBOX_MOD(r4.j + r4.s[r4.i])
+#define ARCFOUR_STEP_5_DROP r4.v1= r4.s[r4.i]; \
+   r4.s[r4.i]= r4.s[r4.j]; r4.s[r4.j]= r4.v1
+#define ARCFOUR_STEP_5_PRNG r4.v1= r4.s[r4.i]; \
+   r4.s[r4.i]= r4.v2= r4.s[r4.j]; r4.s[r4.j]= r4.v1
+#define ARCFOUR_STEP_6_PRNG() r4.s[SBOX_MOD(r4.v1 + r4.v2)]
+#define ARCFOUR_STEP_7_KEY r4.i= SBOX_MOD(r4.i + 1)
